3c6bff6cfeb84f6446a1644bda543d8804462125
Zafar Tagiyev <geko.tm@gmail.com>
2022-05-25 23:11:22 -0600

    added new code

--- main.go
+++ main.go
@@ -3,8 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Print("Get your tickets here to attend")
+	fmt.Printf("conferenceName is %T, remainingTickets is %T and remainingTickets is %T.\n", conferenceName, conferenceTickets, remainingTickets)
+
+	//fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	//fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Printf("We have total of %v tickets and  %v are still available\n", conferenceTickets, remainingTickets)
+	//fmt.Println("Get your tickets here to attend")
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
 }
